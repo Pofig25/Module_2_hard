e1a1549565587d2fdb26b5ea9463c347aa32fc17
Pofig25 <barmindm@gmail.com>
2024-11-12 23:16:29 +1000

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,10 @@
+number = int(input("Введите число от 3 до 20: "))
+result = str()
+#if number < 3 or number > 20:
+#    print("Вы ввели неправильное значение")
+#else:
+for i in range(number):
+    for j in range(i):
+        if i > 0 and j > 0 and number % (i+j) == 0:
+            result += str(i) + str(j)
+print(result)
